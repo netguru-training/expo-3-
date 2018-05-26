1b26dcffcf195562291d68c568ee5099fb80d12e
Robert Gulewicz <nayael@wp.pl>
2018-05-27 00:09:28 +0200

    Add WeatherIcon component

---
+++ app/components/WeatherIcon/WeatherIcon.styles.js
@@ -0,0 +1,12 @@
+const styles = {
+    small: {
+        width: 40,
+        width: 40,
+    },
+    large: {
+        width: 200,
+        width: 200,
+    }
+}
+
+export default styles
--- app/components/index.js
+++ app/components/index.js
@@ -1,9 +1,11 @@
 import CurrentWeatherInfo from './CurrentWeatherInfo/CurrentWeatherInfo'
 import WeatherEventListElement from './WeatherEventListElement/WeatherEventListElement'
 import WeatherHint from './WeatherHint/WeatherHint'
+import WeatherIcon from './WeatherIcon/WeatherIcon'
 
 export {
   CurrentWeatherInfo,
   WeatherEventListElement,
-  WeatherHint
+  WeatherHint,
+  WeatherIcon
 }
