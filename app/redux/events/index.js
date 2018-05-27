08711bbbab3438d11cdc7449ea094964574a68e1
Maciej Jaroszewski <maciejjaroszewski@Offices-MacBook-Pro.local>
2018-05-27 11:51:55 +0200

    Add initial state to keep consistent keys

--- app/redux/events/index.js
+++ app/redux/events/index.js
@@ -1,14 +1,37 @@
 import ActionTypes from "../../actions/types";
 
-const initialState = {
-  1: [],
-  2: [],
-  3: [],
-  4: [],
-  5: [],
-  6: [],
-  7: []
+function addDays(days) {
+  // 86400000 - one day in miliseconds
+  var ms = new Date().getTime() + (86400000 * days);
+  return new Date(ms);
 }
+
+const formatDateNumber = (date) => {
+  const stringDate = `${date}`;
+  if (stringDate.length == 1) {
+    return `0${stringDate}`;
+  }
+  return stringDate;
+}
+
+const generateState = () => {
+  let acc = {};
+  for (let i = 0; i < 7; i++) {
+    const newDate = addDays(i);
+    const day = formatDateNumber(newDate.getDate());
+    const month = formatDateNumber(newDate.getMonth() + 1);
+    const year = formatDateNumber(newDate.getFullYear());
+    const datetime = [year, month, day].join('-');
+    acc = {
+      ...acc,
+      [datetime]: []
+    }
+  }
+
+  return acc;
+}
+
+const initialState = generateState();
 
 const events = (state = initialState, action) => {
   switch (action.type) {
